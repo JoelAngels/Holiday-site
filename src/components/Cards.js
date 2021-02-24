26dad3b95e8d0a4fcc91cec1ec14c93fa6477901
JoelAngels <joelbaraka4325@gmail.com>
2021-02-24 12:35:02 +0300

    Finished styling the cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,7 +10,38 @@ function Cards() {
         <div className="cards__container">
           <div className="cards__wrapper">
             <ul className="cards__items">
-              <CardItem />
+              <CardItem
+                src="image/img-9.jpg"
+                text="Explore the hidden waterfall deep inside the Amazon Jungle"
+                label="Adventure"
+                path="/services"
+              />
+              <CardItem
+                src="image/img-2.jpg"
+                text="Travel through the islands of Bali in a Private Cruise "
+                label="Luxury"
+                path="/services"
+              />
+            </ul>
+            <ul className="cards__items">
+              <CardItem
+                src="image/img-3.jpg"
+                text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
+                label="Mystery"
+                path="/services"
+              />
+              <CardItem
+                src="image/img-4.jpg"
+                text="Experience Football on Top of the Himilayan Mountains"
+                label="Adventure"
+                path="/products"
+              />
+              <CardItem
+                src="image/img-2.jpg"
+                text="Explore the hidden waterfall deep inside the Amazon Jungle"
+                label="Adrenaline"
+                path="/sign-up"
+              />
             </ul>
           </div>
         </div>
